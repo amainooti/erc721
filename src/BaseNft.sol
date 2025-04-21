fa61224e7d95134d0a1ff37de6e42f32a09e78aa
amainooti <amainosamuel1@gmail.com>
2025-04-21 13:01:28 +0100

    added first image for the nft practice

---
+++ src/BaseNft.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BaseNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNFT(address to) public returns (uint256) {}
+
+    function toekenURI(uint256 tokenId) public view override returns (string memory){
+        
+    }
+}
