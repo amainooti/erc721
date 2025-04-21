ed8839bc21fa23c43408a325d15d14aa6d82efc3
amainooti <amainosamuel1@gmail.com>
2025-04-21 17:44:00 +0100

    added and made use of some of the erc721 native functions

--- src/BaseNft.sol
+++ src/BaseNft.sol
@@ -5,15 +5,24 @@ pragma solidity ^0.8.18;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BaseNft is ERC721 {
+    // Keeps tract of how much nft is being minted
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
 
     constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
     }
 
-    function mintNFT(address to) public returns (uint256) {}
+    function mintNft(string memory _tokenURI) public {
+    
+        s_tokenIdToUri[s_tokenCounter] = _tokenURI;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
 
-    function toekenURI(uint256 tokenId) public view override returns (string memory){
-        
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return s_tokenIdToUri[tokenId];
     }
 }
